a5c4c7cab6b78a737f022522551a4092ee20fd11
Anam Hossain <anam.hossain@carsguide.com.au>
2019-12-03 17:37:59 +1100

    Create inventory model

---
+++ app/Inventory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Inventory extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'inventories';
+}
